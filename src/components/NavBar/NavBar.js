66ac756910d985f37ee350a1cd9c6f9ffaf1b0a1
Lourdes Holzman <guadalupe.holzman@gmail.com>
2021-11-05 10:19:18 -0300

    creación de NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,21 @@
+
+export const NavBar = () => {
+
+    
+    return (
+    
+    <header className='header'>
+        <h1>Erde Frisch</h1>
+
+        <nav>
+            <ul>
+                <li>Nosotros</li>
+                <li>Productos</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+
+    </header>
+    )
+}
+

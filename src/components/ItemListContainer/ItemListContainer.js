41acff3bdd1824defada3019db8dbfef676625e9
Lourdes Holzman <guadalupe.holzman@gmail.com>
2021-11-24 19:26:04 -0300

    error

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { pedirDatos } from '../helpers/pedirDatos'
 import { Container } from 'react-bootstrap'
-import { Itemcount } from '../Itemcount/Itemcount'
 import { ProductCart } from '../ProductCart/ProductCart'
+import { ItemList } from '../ItemList/ItemList'
 
 
-export const ItemListContainer = ({greeting}) => {
+
+    export const ItemListContainer = ({greeting}) => {
+    
+        const [loading, setLoading] = useState(false)
+        const [productos, setProductos] = useState([])
+    
+        const { catId } = useParams()
+    
+        useEffect(() => {
+            
+            setLoading(true)
+            pedirDatos()
+                .then( (resp) => {
+    
+                    if (!catId) {
+                        setProductos(resp)
+                    } else {
+                        setProductos( resp.filter( prod => prod.category === catId) )
+                    }
+                })
+                .catch( (error) => {
+                    console.log(error)
+                })
+                .finally(() => {
+                    setLoading(false)
+                })
+    
+        }, [catId])
 
     return (
         <>
         <Container className="text-muted my-5">
             <h3> {greeting} </h3>
         </Container>
+        <ItemList items={productos}/>
         <ProductCart/>
         </>
 
---
+++ src/components/data/stock.js
@@ -0,0 +1,29 @@
+export const stock = [
+    {
+        id: 1,
+        price: 1500,
+        name: "Producto 1",
+        desc: "Lorem ipsum",
+        img: "https://via.placeholder.com/220",
+        category: 'zapatillas',
+        stock: 15
+    },
+    {
+        id: 2,
+        price: 2500,
+        name: "Producto 2",
+        desc: "Lorem ipsum",
+        img: "https://via.placeholder.com/220",
+        category: 'zapatillas',
+        stock: 15
+    },
+    {
+        id: 3,
+        price: 3500,
+        name: "Producto 3",
+        desc: "Lorem ipsum",
+        img: "https://via.placeholder.com/220",
+        category: 'remeras',
+        stock: 15
+    },
+]
